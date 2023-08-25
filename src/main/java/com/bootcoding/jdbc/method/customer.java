72a3ce9d7ad6ad2150f705dca575250a47a953ec
pratu-coder <wadgurepratiksha25@gmail.com>
2023-08-25 22:38:09 +0530

    customer

---
+++ src/main/java/com/bootcoding/jdbc/method/customer.java
@@ -0,0 +1,53 @@
+package com.bootcoding.jdbc.method;
+
+public class customer {
+    private int customerid;
+    private String city;
+    private String email;
+    private int salesmanId;
+    private String name;
+
+
+
+    public int getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSalesmanId() {
+        return salesmanId;
+    }
+
+    public void setSalesmanId(int salesmanId) {
+        this.salesmanId = salesmanId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
